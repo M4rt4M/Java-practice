8866f78d50d04fec99f9c83cc34d3cfb9d34a83f
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-04-15 14:41:49 +0100

    Update string_basics.js

--- string_basics.js
+++ string_basics.js
@@ -3,5 +3,15 @@ var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Inli
 /* 
 This is a multi-line comment.
 I can write as much as I want.
+
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
 */
 console.log(myStr);
