36170864621059642ed82e900cf8483adbfbba5b
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-04-15 14:32:16 +0100

    Create string_basics.js

---
+++ string_basics.js
@@ -0,0 +1,7 @@
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Inline comment
+
+/* 
+This is a multi-line comment.
+I can write as much as I want.
+*/
+console.log(myStr);
