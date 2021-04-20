948c48bec9e774577b185dcdede3029dadbb6d1f
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-04-20 16:00:41 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ function functionName() { //define function
 
 functionName(); //call function
 
-//Multiple arguments
+// Multiple arguments
 function functionWithArgs(x, y) {
   console.log(x+y);
 }
@@ -15,3 +15,9 @@ Variables which are defined outside of a function block have Global scope. This 
 Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
 It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
 */
+
+// Return values 
+function timesFive(num) {
+  return num * 5;
+}
+var answer = timesFive(5); //=25
