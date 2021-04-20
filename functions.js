0ed63c7666d3c5b4c9087a85531ef8d93ed6f0b7
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-04-20 15:53:16 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,3 +10,7 @@ function functionWithArgs(x, y) {
   console.log(x+y);
 }
 functionWithArgs(2,9)
+/* 
+Variables which are defined outside of a function block have Global scope. This means, they can be seen everywhere in your JavaScript code.
+Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
+*/
