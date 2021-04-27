a426f0ed3c6eea0df1696325831ed799162f4947
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-04-27 23:35:39 +0100

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,30 @@
+//In order for JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another (Type Coercion).
+function equalityTest(val1, val2) {
+  if (val1 == val2) {
+     return "Equal";
+  }
+  return "Not equal";
+}
+
+console.log(equalityTest(1, 1)); // Equal
+console.log(equalityTest(1, 2)); // Not equal
+console.log(equalityTest(1, "1")); // Equal
+console.log(equalityTest('3', 3)); // Equal
+
+/*
+Strict equality (===) is the counterpart to the equality operator (==). 
+However, unlike the equality operator, which attempts to convert both values being compared to a common type, 
+the strict equality operator does not perform a type conversion.
+*/
+
+function strictEqualityTest(val1, val2) {
+  if (val1 === val2) {
+     return "Equal";
+  }
+  return "Not equal";
+}
+
+onsole.log(equalityTest(1, 1)); // Equal
+console.log(equalityTest(1, 2)); // Not equal
+console.log(equalityTest(1, "1")); // Not equal
+console.log(equalityTest('3', 3)); // Not equal
