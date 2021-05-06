710f0ce639f958efb0c2b52e43bf624dbdf5321a
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-05-07 00:19:32 +0100

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -136,3 +136,31 @@ function switchOfStuff(val) {
 }
 
 console.log(switchOfStuff(1)); // "stuff"
+
+// Multiple Identical Options in Switch Statements
+
+// If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered.
+
+function sequentialSizes(val) {
+  var answer = "";
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+console.log(sequentialSizes(1)); //"Low"
