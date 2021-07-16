0b17a1e8077a4fb35ae2392c70f5bfe49e7f3976
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-07-17 00:17:33 +0100

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,8 +1,22 @@
 // Iteration
 
+// WHILE
+
 var ourArray = [];
 var i = 0;
 while(i < 5) {
   ourArray.push(i);
   i++;
 }
+
+// FOR
+ /* For loops are declared with three optional expressions separated by semicolons: for (a; b; c), 
+ where a is the intialization statement, b is the condition statement, and c is the final expression. */
+
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+} //ourArray will now have the value [0,1,2,3,4]
+
+
+
