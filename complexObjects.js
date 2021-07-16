122b1809db7f1df2273bd078a2fd13a4cf111c1f
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-07-16 18:14:42 +0100

    Update complexObjects.js

--- complexObjects.js
+++ complexObjects.js
@@ -56,4 +56,46 @@ var ourPets = [
 console.log(ourPets[0].names[1]); //Fluffy
 console.log(ourPets[1].names[0]); //Spot
 
-// Use [] when the keys/properties are stored as values
+// Use [] when the value-key pairs/properties are stored as variables
+
+// ***Record Collection Exercise***
+// Setup
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != "tracks" && value != "") { //If prop isn't tracks and value isn't an empty string, update or set that album's prop to value
+    records[id][prop] = value;
+  } else if (prop == "tracks" && records[id].hasOwnProperty("tracks") == 0) { 
+    //If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it
+      var tracks = [];
+      records[id][prop] = tracks;
+      records[id][prop].push(value);
+  } else if (prop == "tracks" && value != "") { //If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array
+      records[id][prop].push(value);
+  } else if (value == "") {
+    delete records[id][prop]; //If value is an empty string, delete the given prop property from the album
+  }
+  return records; //Your function must always return the entire record collection object
+}
+
+
+
