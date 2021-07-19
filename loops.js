51fa40ab0b935aa5b8523f03ef36dcc3a2509332
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-07-19 16:50:01 +0100

    Update loops.js

--- loops.js
+++ loops.js
@@ -25,3 +25,15 @@ for (var i = 0; i < arr.length; i++) {
    console.log(arr[i]);
 }
 
+// Nesting FOR loops
+
+var arr = [
+  [1,2], [3,4], [5,6]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+
+// DO...WHILE and WHILE
