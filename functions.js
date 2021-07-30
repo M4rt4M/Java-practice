96f5062e55c8af22e449aac0f5392ebd2e1ec211
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-07-31 00:49:22 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -90,3 +90,7 @@ const myFunc = () => { // ...is equivalent to this in ES6: <function name = () =
   return myVar;
 }
 
+/* When there is no function body, and only a return value, arrow function syntax allows you to omit the keyword return 
+as well as the brackets surrounding the code. This helps simplify smaller functions into one-line statements: */
+
+const myFunc = () => "value";
