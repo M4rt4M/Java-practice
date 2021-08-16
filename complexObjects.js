65c93f2be1db81f2664daf694fc24596b25f0a43
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-08-17 00:02:58 +0100

    Update complexObjects.js

--- complexObjects.js
+++ complexObjects.js
@@ -169,4 +169,28 @@ const list = [1, 3, 'a', 'b', 5, 3, 2, 6, 'd'];
 const [,,...arr] = list;
 console.log(arr); //['a', 'b', 5, 3, 2, 6, 'd']
 
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+// ES5
+const profileUpdate = (profileData) => {
+  const { name, age, nationality, location } = profileData;
+}
+
+// ES6
+const profileUpdate = ({ name, age, nationality, location }) => {
+} // When profileData is passed to the above function, the values are destructured from the function parameter for use within the function.
+
+// Example:
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = (stats) => (stats.max + stats.min) / 2.0; //can be simpified to
+const half = ({max, min}) => (max + min) / 2.0; 
+
 
