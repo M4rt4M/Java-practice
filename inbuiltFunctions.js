c2798d2b95f9ae83ae451e0a88cf1b0d5920d5f6
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-08-16 23:53:25 +0100

    Update inbuiltFunctions.js

--- inbuiltFunctions.js
+++ inbuiltFunctions.js
@@ -1,6 +1,8 @@
 Math.random() // generates a random decimal number between 0 (inclusive) and 1 (exclusive)
 Math.floor()
 Math.ceil()
+Math.min()
+Math.max()
 
 var radix = 1; //the base of the number in the string
 parseInt("string", radix); // returns an integer from a string with a base of radix, e.g. parseInt("11", 2) will return 3 because 11 is 3 in binary system
