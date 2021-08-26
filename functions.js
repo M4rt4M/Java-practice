eb96bf85710b340705b5135b09245b8ac7830137
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-08-26 23:49:19 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -173,7 +173,7 @@ const zeus = new SpaceShuttle('Jupiter');
 This constructor is invoked when new is called to create a new object. More to come with OOP.
 [!] UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above. */
 
-// PROMISE
+// PROMISE + THEN + CATCH
 
 /* A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, usually asynchronously. 
 When the task completes, you either fulfill your promise or fail to do so. Promise is a constructor function, so you need to use the new keyword to create one. 
@@ -195,6 +195,12 @@ Often, it might be an object, that you would use data from, to put on your websi
 When you make a server request it takes some amount of time, and after it completes you usually want to do something with the response from the server. 
 This can be achieved by using the <then> method. The <then> method is executed immediately after your promise is fulfilled with resolve. */
 
-myPromise.then(result => { //result comes from the argument given to the resolve method.
+myPromise.then(result => { // <result> comes from the argument given to the resolve method.
   console.log(`The following action has been completed: ${result}`);
+});
+
+// <catch> is the method used when your promise has been rejected. It is executed immediately after a promise's reject method is called. 
+
+myPromise.catch(error => { // <error> is the argument passed in to the reject method.
+  
 });
