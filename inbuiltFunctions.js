831976f99760e1ae29fc79c91ef54ff82db02ddc
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-08-26 23:15:40 +0100

    Update inbuiltFunctions.js

--- inbuiltFunctions.js
+++ inbuiltFunctions.js
@@ -6,3 +6,5 @@ Math.max()
 
 var radix = 1; //the base of the number in the string
 parseInt("string", radix); // returns an integer from a string with a base of radix, e.g. parseInt("11", 2) will return 3 because 11 is 3 in binary system
+
+// Operations on arrays: .map
